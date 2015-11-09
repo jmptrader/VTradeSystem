4e5847c292c7dd5cf4bb3c403412d3dee61d4114
Shiyu Dong <acton@dyn-160-39-144-87.dyn.columbia.edu>
2015-11-08 21:58:04 -0500

    model order

---
+++ src/util/Order.java
@@ -0,0 +1,27 @@
+package util;
+
+
+public class Order {
+	int SenderCompID;
+	String SendingTime;
+	double Price;
+	int Side;
+	String Symbol;
+	String OrderType;
+	int OrderQty;
+	String TimeInForce;
+	String TransacTime;
+	String MaturityMonthYear;
+	String MaturityDay;
+	String MsgType;
+	int ClOrdID;
+	String ExecInst;
+}
+
+
+
+
+
+
+
+
